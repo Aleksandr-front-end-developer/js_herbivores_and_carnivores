46b77f851bf5995284f5cd40e5d8e910a9337f67
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-03-27 11:28:33 +0200

    have done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    Animal.alive.push(this);
+  }
+  bite(animal) {
+    if (!animal.hidden && animal instanceof Herbivore) {
+      if (animal.health === 50) {
+        const deletedAnimalIndex = Animal.alive.indexOf(animal);
+
+        Animal.alive.splice(deletedAnimalIndex, 1);
+      } else {
+        animal.health -= 50;
+      }
+    }
+  }
 }
 
 module.exports = {
